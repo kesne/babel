363398a399d43a86f946167b862ff5220ecc36af
Henry Zhu <hi@henryzoo.com>
2016-02-29 18:10:46 -0500

    Regression with exporting an arrow function with a default param

---
+++ packages/babel-plugin-transform-es2015-modules-commonjs/test/fixtures/regression/T7160/expected.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var foo = exports.foo = function foo(gen) {
+  let ctx = arguments.length <= 1 || arguments[1] === undefined ? null : arguments[1];
+};
+
+var bar = exports.bar = function bar(gen) {
+  let ctx = arguments.length <= 1 || arguments[1] === undefined ? null : arguments[1];
+};
